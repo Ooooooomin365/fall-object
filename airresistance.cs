978010debf000ac401a785af6fd694dea961226a
omin <oooooomindayo1007@gmail.com>
2019-02-15 22:40:18 +0900

    SampleScene.unity airresistance.cs

---
+++ airresistance.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class airresistance : MonoBehaviour {
+	
+	public float keisu;
+	
+	float fallTime = 0;
+	
+	Rigidbody force;
+	
+	void Start() {
+		this.force = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+		force.AddForce(- keisu * force.velocity);
+	}
+	
+	void Update() {
+		fallTime += Time.deltaTime;
+	}
+	
+	void OnCollisionEnter(Collision other){
+		Debug.Log(fallTime);
+	}
+}
